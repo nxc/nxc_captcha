7c2e79ad2ed44175cd1e808babd9b5d2c1acd0c1
David Sayre <david.sayre@beaconfire.com>
2014-02-07 11:23:11 -0500

    Update module/get.php to access settings/nxc_captcha.ini.append.php
    Passing an attributeID '-1' ( /nxc_captcha/get/-1/<session_key>/(regen> ) will allow an image to be created using only the defaults.
    
    This is useful for expanding the NXC Captcha usage beyond object
    attributes and into a general captcha form validator.

--- modules/nxc_captcha/get.php
+++ modules/nxc_captcha/get.php
@@ -4,27 +4,52 @@
  * @author  Serhey Dolgushev <serhey.dolgushev@nxc.no>
  * @date    15 Sep 2011
  **/
-
-$attribute = eZContentClassAttribute::fetch( $Params['classAttributeID'] );
-if( $attribute instanceof eZContentClassAttribute === false ) {
-	echo ezpI18n::tr(
-		'extension/nxc_captcha',
-		'Can`t fetch class attribute (ID: %id)',
-		null,
-		array(
-			'%id' => $Params['classAttributeID']
-		)
-	);
-	eZExecution::cleanExit();
-}
-
-$content = $attribute->attribute( 'content' );
+ 
+/**
+ * Updated to use -1 value for nxc_capture.ini defaults
+ * @author  David Sayre <david@sayre.com>
+ * @date	04 Feb 2014
+ **/
+ 
 $params  = array();
-foreach( $content as $option => $info ) {
-	if( isset( $info['value'] ) ) {
-		$params[ $option ] = $info['value'];
+
+if ($Params['classAttributeID'] == -1 ) {
+	// Generate from INI Defaults
+	$ini = eZINI::instance( 'nxc_captcha.ini' );
+	$params['length'] = $ini->variable( 'CaptchaSettings', 'Length' );
+	$params['type'] = $ini->variable( 'CaptchaSettings', 'Type' );
+	$params['width'] = $ini->variable( 'CaptchaSettings', 'Width' );
+	$params['height'] = $ini->variable( 'CaptchaSettings', 'Height' );
+	$params['noise_level'] = $ini->variable( 'CaptchaSettings', 'NoiseLevel' );
+	$params['text_tilt_angle'] = $ini->variable( 'CaptchaSettings', 'TextTiltAngle' );
+	$params['characters_color'] = $ini->variable( 'CaptchaSettings', 'CharactersColor' );
+	$params['background_color'] = $ini->variable( 'CaptchaSettings', 'BackgroundColor' );
+	$params['noise_color'] = $ini->variable( 'CaptchaSettings', 'NoiseColor' );
+	$params['skip_user_ids'] = $ini->variable( 'CaptchaSettings', 'SkipUserIds' );
+	$params['skip_role_ids'] = $ini->variable( 'CaptchaSettings', 'SkipRoleIds' );
+	$params['exclude_characters'] = $ini->variable( 'CaptchaSettings', 'ExcludeCharacters' );
+} else {
+	// Generate from Object Attribute
+	$attribute = eZContentClassAttribute::fetch( $Params['classAttributeID'] );
+	if( $attribute instanceof eZContentClassAttribute === false ) {
+		echo ezpI18n::tr(
+			'extension/nxc_captcha',
+			'Can`t fetch class attribute (ID: %id)',
+			null,
+			array(
+				'%id' => $Params['classAttributeID']
+			)
+		);
+		eZExecution::cleanExit();
+	}
+	$content = $attribute->attribute( 'content' );
+	foreach( $content as $option => $info ) {
+		if( isset( $info['value'] ) ) {
+			$params[ $option ] = $info['value'];
+		}
 	}
 }
+
 if( isset( $params['exclude_characters'] ) ) {
 	$params['exclude_characters'] = explode( ',', $params['exclude_characters'] );
 }
